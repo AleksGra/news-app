7781f7965259bdc27cb6c5d781e5becd3372998a
devid <devidead@mail.ru>
2022-09-03 11:24:54 +0200

    without inf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header/Header";
-import {Search} from "./components/Search/Search";
 import {Main} from "./components/Main/Main";
 
 function App() {
