0a613f642dc0d28bf6da998ec6a5f4a2eabe98f1
devid <devidead@mail.ru>
2022-09-03 11:30:44 +0200

    without inf

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -4,13 +4,9 @@ import styles from './news-list.module.css'
 
 
 const NewsList = ({newsList}) => {
-    return (
-        <div className={styles.wrapper}>
-            {newsList.map((news, index) => (
-                <NewsListItem news={news} key={index}/>
-            ))}
-        </div>
-    )
+    return <div className={styles.wrapper}>
+        {newsList.map((news, index) => <NewsListItem news={news} key={index}/>)}
+    </div>
 }
 
 export default NewsList;
